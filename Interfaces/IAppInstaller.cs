712a73a72e58e903ed97fb3688f48e5fbaa504a1
korposs <111388197+korposs@users.noreply.github.com>
2023-12-29 22:29:35 +0700

    Add files via upload

---
+++ Interfaces/IAppInstaller.cs
@@ -0,0 +1,9 @@
+﻿namespace Phonestore.Interfaces
+{
+    public interface IAppInstaller
+    {
+        void InstallApp(IInstallable app);
+        void RemoveApp(IInstallable app);
+        bool IsInstalled(IInstallable app);
+    }
+}
